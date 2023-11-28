3010bdf1908a0712c3a6d5e0e3ccd37895410720
Sai Kashyap <kashyap.bsk99@gmail.com>
2023-11-28 20:00:37 +0530

    .

---
+++ home/home.js
@@ -0,0 +1,27 @@
+const wrapper = document.querySelector(".wrapper");
+const registerlink = document.querySelector(".register-link");
+const loginlink = document.querySelector(".login-link");
+const btnpopup = document.querySelector(".btnlogin-poppin");
+const iconclose = document.querySelector(".cross");
+registerlink.addEventListener("click", () => {
+  wrapper.classList.add("active");
+});
+
+loginlink.addEventListener("click", () => {
+  wrapper.classList.remove("active");
+});
+
+btnpopup.addEventListener("click", () => {
+  wrapper.classList.add("active-popup");
+});
+
+iconclose.addEventListener("click", () => {
+  wrapper.classList.remove("active-popup");
+});
+
+// JavaScript code to handle the button click event
+document.getElementById("linkButton").addEventListener("click", function () {
+  window.location.href = "features.html";
+});
+
+/*firebase*/
