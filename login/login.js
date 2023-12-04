71df33910547eae8dd61969e9631a2a10a5ab523
Sai Kashyap <kashyap.bsk99@gmail.com>
2023-12-04 12:46:56 +0530

    .

--- login/login.js
+++ login/login.js
@@ -24,3 +24,20 @@ document.getElementById("linkButton").addEventListener("click", function () {
   window.location.href = "features.html";
 });
 
+
+const maxChecks = 1
+let selectedCount = 0
+
+document.querySelector('patient-box').addEventListener('click', event => {
+  if (event.target.type === 'checkbox') {
+    selectedCount = event.target.checked
+      ? selectedCount + 1
+      : selectedCount - 1
+  }
+
+  if (selectedCount >= maxChecks) {
+    [...document.querySelectorAll('input:not(:checked)')].forEach(input => input.disabled = true)
+  } else {
+    [...document.querySelectorAll('input')].forEach(input => input.disabled = false)
+  }
+})
