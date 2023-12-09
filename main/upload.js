af0f9756dabd7ca5004909e35884d80aeb003b97
Sai Kashyap <kashyap.bsk99@gmail.com>
2023-12-09 11:35:37 +0530

    new

--- main/upload.js
+++ main/upload.js
@@ -1,60 +1,57 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
+const Moralis = require("moralis").default;
+const fs = require("fs");
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
 
-// Set up multer for handling file uploads
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Specify the upload directory
+    cb(null, 'uploads/');
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname); // Use the original file name
+    cb(null, file.originalname); 
   }
 });
 
 const upload = multer({ storage: storage });
 
-// Serve the HTML form
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'main.html'));
 });
 
-// Handle file uploads
-app.post('/upload', upload.single('file'), (req, res) => {
-  const filePath = req.file.path; // This is the server-side path where the file is stored
-//   res.send(File uploaded to: ${filePath});
-// });
 
-const Moralis = require("moralis").default;
-const fs = require("fs");
-require("dotenv").config();
+app.post('/upload', upload.single('file'), async (req, res) => {
+  const filePath = req.file.path; 
 
-const fileUploads = [
+  res.send(`File uploaded to: ${filePath}`);
+
+  const fileUploads = [
     {
-        path : filePath,
-        content: fs.readFileSync(filePath,{encoding: "base64"})
+      path: filePath,
+      content: fs.readFileSync(filePath, { encoding: "base64" })
     }
-]
+  ];
 
-async function uploadToIpfs(){
-    await Moralis.start({
-        apiKey : process.env.MORALIS_KEY 
-    })
-
-    const res = await Moralis.EvmApi.ipfs.uploadFolder({
-        abi : fileUploads
-    })
-
-    console.log(res.result);
-}
-
-uploadToIpfs();
-
+  await uploadToIpfs(fileUploads);
 });
 
+async function uploadToIpfs(fileUploads) {
+  await Moralis.start({
+    apiKey: process.env.MORALIS_KEY
+  });
+
+  const res = await Moralis.EvmApi.ipfs.uploadFolder({
+    abi: fileUploads
+  });
+
+  console.log(res.result);
+}
+
 app.listen(port, () => {
-  console.log(Server is running on http://localhost:${port});
+  console.log(`Server is running on http://localhost:${port}`);
 });
