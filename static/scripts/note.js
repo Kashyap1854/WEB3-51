fc6a748dfd449d57ae7cdeb7fe81edc46ee38e2c
Sai Kashyap <kashyap.bsk99@gmail.com>
2023-12-20 14:23:30 +0530

    new

--- static/scripts/note.js
+++ static/scripts/note.js
@@ -1,20 +1,45 @@
+window.onload = function () {
+    let btn = document.querySelector("#btn");
+    let sidebar = document.querySelector(".sidebar");
+    let searchBtn = document.querySelector(".bx-search");
+    let listitem = document.querySelectorAll(".list-item");
 
-      function saveText() {
-        var textInput = document.getElementById('textInput').value;
-        localStorage.setItem('savedText', textInput);
-        alert('Text saved successfully!');
-    }
-
-
-    function clearText() {
-        document.getElementById('textInput').value = '';
-        localStorage.removeItem('savedText');
-        alert('Text cleared successfully!');
-    }
-
-    window.onload = function () {
-        var savedText = localStorage.getItem('savedText');
-        if (savedText) {
-            document.getElementById('textInput').value = savedText;
-        }
+    btn.onclick = function () {
+        sidebar.classList.toggle("active");
     };
+
+    searchBtn.onclick = function () {
+        sidebar.classList.toggle("active");
+    };
+
+    function activeLink() {
+        listitem.forEach((item) => item.classList.remove("active"));
+        this.classList.add("active");
+    }
+
+    listitem.forEach((item) => (item.onclick = activeLink));
+};
+/*-------------------*/
+function saveNote() {
+    const textInput = document.getElementById('textInput');
+    const colorPicker = document.getElementById('colorPicker');
+
+    const noteContainer = document.getElementById('savedNotes');
+    const note = document.createElement('div');
+    note.className = 'note';
+    note.style.backgroundColor = colorPicker.value;
+    note.innerHTML = textInput.value;
+
+    noteContainer.appendChild(note);
+    clearText();
+}
+
+function clearText() {
+    document.getElementById('textInput').value = '';
+}
+
+function clearNotes() {
+    const noteContainer = document.getElementById('savedNotes');
+    noteContainer.innerHTML = '';
+}
+
