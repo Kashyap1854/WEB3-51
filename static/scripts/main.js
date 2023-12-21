51b4b10d62d6b234997bb5ef3f29d7cd4afe3e48
Sai Kashyap <kashyap.bsk99@gmail.com>
2023-12-21 07:20:21 +0530

    new

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -10,3 +10,21 @@ function logout() {
     form.submit();
   }
 }
+
+let btn = document.querySelector("#btn");
+let sidebar = document.querySelector(".sidebar");
+let searchBtn = document.querySelector(".bx-search");
+let listitem = document.querySelectorAll(".list-item");
+btn.onclick = function () {
+  sidebar.classList.toggle("active");
+};
+
+searchBtn.onclick = function () {
+  sidebar.classList.toggle("active");
+};
+
+function activeLink() {
+  listitem.forEach((item) => item.classList.remove("active"));
+  this.classList.add("active");
+}
+listitem.forEach((item) => (item.onclick = activeLink));
